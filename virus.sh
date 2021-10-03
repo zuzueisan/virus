48218f92e821c3b69ec0ed5850c7caf60f8da0a8
zuzueisan <91845141+zuzueisan@users.noreply.github.com>
2021-10-04 00:45:25 +0630

    Add files via upload

---
+++ virus.sh
@@ -0,0 +1,382 @@
+# A tool by M.A.H.T
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+banner(){
+vid
+echo -e '\e[93m               
+          ⣿⡇⣿⣿⣿⠛⠁⣴⣿⡿⠿⠧⠹⠿⠘⣿⣿⣿⡇⢸⡻⣿⣿⣿⣿⣿⣿⣿
+          ⢹⡇⣿⣿⣿⠄⣞⣯⣷⣾⣿⣿⣧⡹⡆⡀⠉⢹⡌⠐⢿⣿⣿⣿⡞⣿⣿⣿
+          ⣾⡇⣿⣿⡇⣾⣿⣿⣿⣿⣿⣿⣿⣿⣄⢻⣦⡀⠁⢸⡌⠻⣿⣿⣿⡽⣿⣿
+          ⡇⣿⠹⣿⡇⡟⠛⣉⠁⠉⠉⠻⡿⣿⣿⣿⣿⣿⣦⣄⡉⠂⠈⠙⢿⣿⣝⣿
+          ⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮
+          ⠄⢸⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀
+         ⠄⠈⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸
+         ⠄⠄⢿⣿⡀⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡾⠁⢠⡇⢀
+         ⠄⠄⢸⣿⡇⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣫⣻⡟⢀⠄⣿⣷⣾
+         ⠄⠄⢸⣿⡇⠄⠈⠙⠿⣿⣿⣿⣮⣿⣿⣿⣿⣿⣿⣿⣿⡿⢠⠊⢀⡇⣿⣿
+         ⠒⠤⠄⣿⡇⢀⡲⠄⠄⠈⠙⠻⢿⣿⣿⠿⠿⠟⠛⠋⠁⣰⠇⠄⢸⣿⣿⣿
+         ⠄⠄⠄⣿⡇⢬⡻⡇⡄⠄⠄⠄⡰⢖⠔⠉⠄⠄⠄⠄⣼⠏⠄⠄⢸⣿⣿⣿
+         ⠄⠄⠄⣿⡇⠄⠙⢌⢷⣆⡀⡾⡣⠃⠄⠄⠄⠄⠄⣼⡟⠄⠄⠄⠄⢿⣿⣿
+'
+echo " " 
+echo -e "$red          ▶Coded by$grn Myanmar Anonymous Helper Team $red◀$rset"
+echo -e "$red                      ⫸$ylo  N42 $red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [M.A.H.T them with fun]$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e  '\e[93m               
+          ⣿⡇⣿⣿⣿⠛⠁⣴⣿⡿⠿⠧⠹⠿⠘⣿⣿⣿⡇⢸⡻⣿⣿⣿⣿⣿⣿⣿
+          ⢹⡇⣿⣿⣿⠄⣞⣯⣷⣾⣿⣿⣧⡹⡆⡀⠉⢹⡌⠐⢿⣿⣿⣿⡞⣿⣿⣿
+          ⣾⡇⣿⣿⡇⣾⣿⣿⣿⣿⣿⣿⣿⣿⣄⢻⣦⡀⠁⢸⡌⠻⣿⣿⣿⡽⣿⣿
+          ⡇⣿⠹⣿⡇⡟⠛⣉⠁⠉⠉⠻⡿⣿⣿⣿⣿⣿⣦⣄⡉⠂⠈⠙⢿⣿⣝⣿
+          ⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮
+          ⠄⢸⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀
+         ⠄⠈⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸
+         ⠄⠄⢿⣿⡀⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡾⠁⢠⡇⢀
+         ⠄⠄⢸⣿⡇⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣫⣻⡟⢀⠄⣿⣷⣾
+         ⠄⠄⢸⣿⡇⠄⠈⠙⠿⣿⣿⣿⣮⣿⣿⣿⣿⣿⣿⣿⣿⡿⢠⠊⢀⡇⣿⣿
+         ⠒⠤⠄⣿⡇⢀⡲⠄⠄⠈⠙⠻⢿⣿⣿⠿⠿⠟⠛⠋⠁⣰⠇⠄⢸⣿⣿⣿
+         ⠄⠄⠄⣿⡇⢬⡻⡇⡄⠄⠄⠄⡰⢖⠔⠉⠄⠄⠄⠄⣼⠏⠄⠄⢸⣿⣿⣿
+         ⠄⠄⠄⣿⡇⠄⠙⢌⢷⣆⡀⡾⡣⠃⠄⠄⠄⠄⠄⣼⡟⠄⠄⠄⠄⢿⣿⣿
+
+'
+
+echo " " 
+echo -e "$red                        ▶Coded by$grn N42 $red◀$rset"
+echo -e "$red             ⫸$ylo  Myanmar Anonymous Helper Team$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [M.A.H.T them with fun]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn             ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1\e[96m] Virus 1"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] Virus 2"
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] About and M.A.H.T Lesson couse ! "
+echo -e "$red                        ➡$cyan [\e[92m4\e[96m] Update script "
+echo -e "$red                        ➡$cyan [\e[92m5\e[96m] Subscribe"
+echo -e "$red                        ➡$cyan [\e[92m6\e[96m] More"
+echo -e "$red                        ➡$cyan [\e[92m7\e[96m] Ethical Hacking Knowledge "
+echo -e "$red                        ➡$cyan [\e[92m8\e[96m] Exit"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+about
+elif [ $optnz = "4" ];
+then
+upd
+elif [ $optnz = "5" ];
+then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "6" ];
+then
+am start -a android.intent.action.VIEW -d https://noob-hackers.github.io/noobspage > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "7" ];
+then
+am start -a android.intent.action.VIEW -d https://t.me/termuxtool0033 > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "8" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+################
+clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
+echo " "
+echo -e "\e[91m 😈  Copy below link and send it your victim and 
+say nothing to him. when he install it in his 
+device it will be factory reset and lot more happens.
+                                👇👇👇
+         \e[92mL1NK :- \e[96mhttps://bit.ly/3ild93L\e[91m
+                                 👆👆👆
+Use only for fun purpose not for harming someone 😇✌️.
+And our channel is not responsible for any kind of loss 
+you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
+echo " "
+echo " "
+echo -e "\e[93m  Copy below link and send it your victim and tell him
+that he should install this application or else his 
+device will not be recovered.(Antivirus)
+                    👇👇👇
+        \e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
+                    👆👆👆
+Use only for fun purpose not for harming someone 😇✌️. 
+And our channel is not responsible for any kind of 
+loss you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+         ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐────
+'
+echo ""
+
+echo -e "\e[1;32m     It is written for the development of Burma and for the development of Burman nationalism.We are the Myanmar anonymous reporter & recovery team (M.A.H.R.T / M.A.H.T).   \e[0m"
+
+echo -e "\e[1;31m
+
+🍃🌹🍃💕
+💕          💕🍃🌹🍃💕
+💕                💕🍃
+💕    ( N42 )    💕
+💕`    💗    ` 💕
+💕           💕 
+💕            💕 
+💕             💕
+💕( M.A.H.T ) 💕
+💕`   💗  ` 💕
+💕      💕 
+💕   💕 
+💕 💕
+
+
+\e[0m"
+
+echo ""
+
+echo -e "\e[1;32m        Code By Hackcat zuzueisan(N42)  \e[0m"
+
+echo ""
+
+echo -e "\e[1;32m M.A.H.T Lesson Course! \e[0m"
+
+echo ""
+
+echo " 1. Report & Lock ( movie lesson ) 15000 kyat "
+echo""
+echo " 2. Report & Lock ( letter lesson ) 15000 kyat"
+echo ""
+echo " 3. Facebook ADS Boost Lesson ( movie lesson ) 5000 kyat"
+echo ""
+echo " 4. Hacking Basic Knowledge Lesson  5000 kyat"
+echo ""
+echo " 5. Tool Owner create Phishing Lesson 5000 kyat"
+echo ""
+echo " 6. Android Development Phishing Lesson 10000 kyat"
+echo ""
+echo " 7. Android Studio Lesson for Pc 10000 kyat"
+echo ""
+echo " 8. Php language 10000 kyat"
+
+echo ""
+read -p " 😈Choose what you want to Click : " ghost
+
+if [[ $ghost == 1 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/536338961017855/?ref=share
+
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 2 ]];then
+echo ""
+sleep 3
+termux-open  https://www.facebook.com/groups/4371575249554564/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 3 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/352947832964336/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 4 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/4144238072297742/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 5 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/4144238072297742/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 6 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/533944837706828/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+
+
+
+
+else
+echo -e "\e[1;33m Wellcome to Myanmar Anonymous Helper Team \e[0m"
+fi
+
+
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/infect" ];
+then
+cd $HOME
+rm -rf infect
+elif [ -d "$HOME/Infect" ];
+then
+cd $HOME
+rm -rf Infect
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/noob-hackers/infect 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m           UPDATE SUCCESSFULL (LATEST VERSION)..!\e[0m"
+sleep 2.0
+cd $HOME
+cd infect
+bash infect.sh
+}
+vid(){
+FILE=$HOME/infect/noob.noob
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+echo -e "\e[96m                ╔═══════════════════════════════════╗\e[0m"
+echo -e "\e[96m                ║  \e[93mMyanmar Anonymous Helper \e[96m Team   ║\e[0m"
+echo -e "\e[96m                ║Tool creater facebook account go?  ║\e[0m"
+echo -e "\e[96m                ║                                   ║\e[0m"
+echo -e "\e[96m                ║        Select \e[92my\e[96m to watch\e[96m          ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║        Select \e[91mt\e[96m to cancel\e[96m         ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║     Select \e[91mz\e[96m to skipforever\e[96m       ║\e[0m"
+echo -e "\e[96m                ╚═══════════════════════════════════╝\e[0m"
+echo -en "\e[32mSELECT OPTION [\e[93my/\e[93mt/\e[32m\e[93mz\e[32m]: \e[0m "
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d https://m.facebook.com/account_status/?location=profile_self 2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOM/infect
+rm noob.noob
+banner
+menu
+else
+banner
+menu
+exit
+fi
+}
+banner
+menu
